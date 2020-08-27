58287964156b461ffed830516af5994b0442ad92
Nimrah Mustafa <nimrahmustafa@Nimrah-Mustafa.local>
2020-08-27 13:28:05 +0500

    removing folders

--- localnative-electron/src/locales/zh-CN.js
+++
@@ -1,47 +0,0 @@
-ï»¿/*
-    Local Native
-    Copyright (C) 2018-2019  Yi Wang
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-let locales = {};
-
-locales['zh-CN'] = i18n.create({
-  values: {
-    "ssbify": 'ä¿å­åå®¹å° ssb',
-    "public": 'å¬å¼',
-    "Language": 'è¯­è¨',
-    "Sync Between Devices": 'åæ­¥è®¾å¤',
-    "sync via attach file": 'éè¿æä»¶åæ­¥',
-    "Server": 'æå¡å¨',
-    "start server": 'å¯å¨æå¡å¨',
-    "stop server": 'å³é­æå¡å¨',
-    "Client": 'å®¢æ·ç«¯',
-    "start client sync": 'å¯å¨å®¢æ·ç«¯åæ­¥',
-    "description": "æè¿°",
-    "title": 'æ é¢',
-    "url": 'url é¾æ¥',
-    "type to add tags, enter to save, comma or space as tag seperator": 'è¾å¥æ ç­¾ï¼åè½¦ä¿å­ï¼åè§éå·æç©ºæ ¼ä¸ºæ ç­¾çåéç¬¦',
-    "type to search": 'è¾å¥æç´¢',
-    "clear search term(s)": 'æ¸é¤æç´¢æ¡',
-    "prev": 'ä¸é¡µ',
-    "next": 'ä¸é¡µ',
-    "page": 'é¡µ',
-    "of": 'å±',
-    "ssb sync": 'ssb åæ­¥',
-    "Tags": 'æ ç­¾',
-    "Timeseries Charts": 'æ¶é´åºåå¾',
-    "select a time range to zoom in": 'éæ©è¦æ¾å¤§çæ¶é´èå´'
-  }
-});
